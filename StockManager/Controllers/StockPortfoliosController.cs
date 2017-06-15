e7183f40b7b0de1720914b15d420b5ba4cf75050
Jimmy Lundberg <jimmy-lundberg@hotmail.com>
2017-06-15 13:35:35 +0200

    Added authorization attributes in StocksController and StockPortfoliosController.

--- StockManager/Controllers/StockPortfoliosController.cs
+++ StockManager/Controllers/StockPortfoliosController.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using StockManager.Data;
 using StockManager.Models;
 
 namespace StockManager.Controllers
 {
+    [Authorize]
     public class StockPortfoliosController : Controller
     {
         private readonly ApplicationDbContext _context;
--- StockManager/Controllers/StocksController.cs
+++ StockManager/Controllers/StocksController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using StockManager.Data;
 using StockManager.Models;
@@ -44,6 +45,7 @@ namespace StockManager.Controllers
         }
 
         // GET: Stocks/Create
+        [Authorize]
         public IActionResult Create()
         {
             return View();
@@ -53,6 +55,7 @@ namespace StockManager.Controllers
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
+        [Authorize]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Name")] Stock stock)
         {
@@ -66,6 +69,7 @@ namespace StockManager.Controllers
         }
 
         // GET: Stocks/Edit/5
+        [Authorize]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -85,6 +89,7 @@ namespace StockManager.Controllers
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
+        [Authorize]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Stock stock)
         {
@@ -117,6 +122,7 @@ namespace StockManager.Controllers
         }
 
         // GET: Stocks/Delete/5
+        [Authorize]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -136,6 +142,7 @@ namespace StockManager.Controllers
 
         // POST: Stocks/Delete/5
         [HttpPost, ActionName("Delete")]
+        [Authorize]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
